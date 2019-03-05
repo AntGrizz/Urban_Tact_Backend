d4f18b7a82f8950319499999bb232db8932f00d0
Anthony Gregg <agregg22@gmail.com>
2019-03-05 09:37:12 -0500

    setup fetch and class constructor

--- ut_javascript_frontend/src/index.js
+++ ut_javascript_frontend/src/index.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-alert('LOADED')
-
-const endPoint = 'http://localhost:3000/api/v1/users';
-  fetch(endPoint)
-    .then(res => res.json())
-    .then(json => console.log(json));
+  init();
 });
+
+const usersUrl = 'http://localhost:3000/users';
+
+const postsUrl = 'http://localhost:3000/posts';
+
+function init() {
+  getUsers();
+  getPosts();
+}
+
+function getUsers() {
+  fetch(usersUrl)
+    .then(res => res.json())
+    .then(users => console.log(users));
+}
+
+function getPosts() {
+  fetch(postsUrl)
+    .then(res => res.json())
+    .then(posts => console.log(posts));
+}
--- ut_rails_backend/config/routes.rb
+++ ut_rails_backend/config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-namespace :api do
-    namespace :v1 do
       resources :users, only: [:index, :update]
-    end
-  end
-
-namespace :api do
-    namespace :v1 do
       resources :posts, only: [:index, :update]
-    end
-  end
 end
