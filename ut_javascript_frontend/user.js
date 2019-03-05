802ba5799ac06b3fda3e359af0535554b333640a
Anthony Gregg <agregg22@gmail.com>
2019-03-05 10:08:07 -0500

    CSS

---
+++ ut_javascript_frontend/user.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(post) {
+    this.id = user.id;
+    this.username = user.username;
+    this.email = user.email;
+    this.image = user.image;
+    Post.all.push(this);
+  }
+
+  renderUsers() {
+    document;
+  }
+}
+
+User.all = [];
---
+++ ut_rails_backend/app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+ def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def update
+    @post.update(post_params)
+    if @post.save
+      render json: @post, status: :accepted
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content, :description, :image, :video, :link, :tags, :likes)
+  end
+
+
+end
