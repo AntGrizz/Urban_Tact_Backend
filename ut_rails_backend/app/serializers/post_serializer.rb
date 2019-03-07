bd3c727c90d03d7ba005604d77e1a0af4efd3663
gillsinaglass <rdgill95@gmail.com>
2019-03-07 12:11:09 -0500

    Serialized

--- ut_rails_backend/app/serializers/post_serializer.rb
+++ ut_rails_backend/app/serializers/post_serializer.rb
@@ -1,4 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :description, :image, :video, :link, :tags, :created_at, :updated_at, :likes 
-  belongs_to :user
+  attributes :id, :title, :content, :description, :image, :video, :link, :tags, :created_at, :updated_at, :likes, :user_id
 end
